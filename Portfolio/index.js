4aa4f13239ac6457e78bb4d796c5ea8bd1f1a045
Natanim Issa <natanimissa@gmail.com>
2023-12-07 10:32:38 -0500

    done contact page

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,47 +1,46 @@
-const btnResume = document.getElementById("btn-resume");
-
-btnResume.addEventListener("click", function () {
+function shouResume() {
+  const btnResume = document.getElementById("btn-resume");
   window.open("./assets/files/NatanimResume.pdf", "_blank");
-});
+}
 
 const projectImg = document.getElementById("project-img");
-const p1 = document.getElementById("p1");
-const p2 = document.getElementById("p2");
-const p3 = document.getElementById("p3");
-const p4 = document.getElementById("p4");
-const p5 = document.getElementById("p5");
 
-p1.addEventListener("mouseenter", function () {
+function showp1() {
   projectImg.style.backgroundImage = 'url("./assets/images/comingsoon.avif")';
-});
-p1.addEventListener("mouseleave", function () {
-  projectImg.style.backgroundImage = "";
-});
+}
 
-p2.addEventListener("mouseenter", function () {
+function showp2() {
   projectImg.style.backgroundImage = 'url("./assets/images/adoption.png")';
-});
-p2.addEventListener("mouseleave", function () {
-  projectImg.style.backgroundImage = "";
-});
+}
 
-p3.addEventListener("mouseenter", function () {
+function showp3() {
   projectImg.style.backgroundImage = 'url("./assets/images/dabinventory.png")';
-});
-p3.addEventListener("mouseleave", function () {
-  projectImg.style.backgroundImage = "";
-});
+}
 
-p4.addEventListener("mouseenter", function () {
+function showp4() {
   projectImg.style.backgroundImage = 'url("./assets/images/prison.png")';
-});
-p4.addEventListener("mouseleave", function () {
-  projectImg.style.backgroundImage = "";
-});
+}
 
-p5.addEventListener("mouseenter", function () {
+function showp5() {
   projectImg.style.backgroundImage = 'url("./assets/images/efda.png")';
-});
-p5.addEventListener("mouseleave", function () {
+}
+
+function hide() {
   projectImg.style.backgroundImage = "";
-});
+}
+
+const items = document.querySelectorAll(".sub-container button");
+
+function toggleResponse() {
+  const item = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (item == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleResponse));
