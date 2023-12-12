4452676fc68eedee0215be7cb533ca5f11dfb4dc
Natanim Issa <natanimissa@gmail.com>
2023-12-12 00:27:45 -0500

    project section added

---
+++ Portfolio/project.js
@@ -0,0 +1,54 @@
+const p1Images = document.querySelectorAll(".pr1-img");
+
+p1Images.forEach((img) => {
+  img.addEventListener("mouseenter", () => {
+    img.style.opacity = "0";
+  });
+  img.addEventListener("mouseleave", () => {
+    img.style.opacity = "";
+  });
+});
+
+const p2Images = document.querySelectorAll(".pr2-img");
+
+p2Images.forEach((img) => {
+  img.addEventListener("mouseenter", () => {
+    img.style.opacity = "0";
+  });
+  img.addEventListener("mouseleave", () => {
+    img.style.opacity = "";
+  });
+});
+
+const p3Images = document.querySelectorAll(".pr3-img");
+
+p3Images.forEach((img) => {
+  img.addEventListener("mouseenter", () => {
+    img.style.opacity = "0";
+  });
+  img.addEventListener("mouseleave", () => {
+    img.style.opacity = "";
+  });
+});
+
+const p4Images = document.querySelectorAll(".pr4-img");
+
+p4Images.forEach((img) => {
+  img.addEventListener("mouseenter", () => {
+    img.style.opacity = "0";
+  });
+  img.addEventListener("mouseleave", () => {
+    img.style.opacity = "";
+  });
+});
+
+const p5Images = document.querySelectorAll(".pr5-img");
+
+p5Images.forEach((img) => {
+  img.addEventListener("mouseenter", () => {
+    img.style.opacity = "0";
+  });
+  img.addEventListener("mouseleave", () => {
+    img.style.opacity = "";
+  });
+});
